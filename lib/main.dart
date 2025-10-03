5626e98f56693aec088cf0b0ba814f59d8f57c2c
Faiyaz <faiyaz.abrar.fa@gmail.com>
2025-10-04 03:12:12 +0600

    ID Card Generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/id_card_widget.dart';
+import 'package:flutter_application_1/id_card_form_page.dart'; // Import IdCardFormPage
 
 void main() {
   runApp(const MyApp());
@@ -8,21 +9,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ID Card Replica',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const IdCardWidget(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const IdCardFormPage(),
+        '/idCard': (context) {
+          final args = ModalRoute.of(context)!.settings.arguments as IdCardData;
+          return IdCardWidget(cardData: args);
+        },
+      },
     );
   }
 }
-
-
-
-
-
-  
